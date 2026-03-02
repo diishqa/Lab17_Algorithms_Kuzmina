9f820edc084a54ac5dca5a03b35d6924b4f7835f
diishqa <diana.34@list.ru>
2026-03-02 20:02:05 +0300

    aa

---
+++ code/step7_arrayElementRun/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Вывод элементов массива");
+int[] grades = { 5, 4, 5, 3, 4 };
+
+Console.WriteLine("Оценки студента:");
+for (int i = 0; i < grades.Length; i++)
+    {
+        Console.WriteLine($"Индекс {i}: {grades[i]}");
+    }
