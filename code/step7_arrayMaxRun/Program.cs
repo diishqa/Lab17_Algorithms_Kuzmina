b7b2c3ccfbabbca254e5cc5db85285373dd2304b
diishqa <diana.34@list.ru>
2026-03-02 20:01:31 +0300

    Step 7 arrays

---
+++ code/step7_arrayMaxRun/Program.cs
@@ -0,0 +1,10 @@
+﻿int[] grades = { 5, 4, 5, 3, 4 };
+int max = grades[0];
+for (int i = 1; i < grades.Length; i++)
+    {
+        if (grades[i] > max)
+        {
+            max = grades[i];
+        }
+    }
+Console.WriteLine("Максимальная оценка: " + max);
